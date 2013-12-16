0752eef37e2878e808b46119a2756dbf0258b414
helkarakse <nimcuron@gmail.com>
2013-12-16 15:05:16 +0800

    Added the create_ticket function and code to the command handler

--- src/php/ticket.php
+++ src/php/ticket.php
@@ -29,6 +29,16 @@
 
 			case "add_ticket" :
 				echo("add_ticket");
+				$creator = isset($_GET["creator"]) ? $_GET["creator"] : "";
+				$description = isset($_GET["description"]) ? $_GET["description"] : "";
+				$position = isset($_GET["position"]) ? $_GET["position"] : "";
+
+				if ($creator != "" && $description != "") {
+					createTicket($creator, $description, $position);
+				} else {
+					echo("Some fields are missing!");
+				}
+				
 				break;
 		}
 	}
@@ -37,11 +47,12 @@
 	function initDb() {
 		// init the db connection
 		$db = new SQLite3($dbPath) or die("Unable to create database.");
-		
+
 		// create table if not already created
-		$db -> exec("CREATE TABLE IF NOT EXISTS tickets (id INTEGER PRIMARY KEY ASC, 
+		$db -> exec("CREATE TABLE IF NOT EXISTS Tickets (id INTEGER PRIMARY KEY ASC, 
 						creator TEXT NOT NULL, 
-						description TEXT NOT NULL, 
+						description TEXT NOT NULL,
+						position TEXT,
 						status INTEGER, 
 						type INTEGER, 
 						notes TEXT, 
@@ -50,11 +61,26 @@
 	}
 
 	function createTicket($creator, $description, $position) {
+		/*
+		 * Enum for status:
+		 * 0 - unread
+		 * 1 - open
+		 * 2 - in progress
+		 * 3 - completed
+		 * 4 - cancelled / rejected
+		 *
+		 * Enum for type
+		 * 1 - mod
+		 * 2 - admin
+		 */
 
+		$create_date = date('Y-m-d H:i:s');
+		$db -> exec("INSERT INTO Tickets VALUES(NULL, '$creator', '$description', '$position', '0', '1', '$create_date', '$create_date')") or die("Failed to create ticket.");
 	}
 
-	function getTickets($filter) {
-
+	function getTickets() {
+		$result = $db -> query('SELECT * FROM Tickets');
+		var_dump($result);
 	}
 
 	function getTicket($id) {
