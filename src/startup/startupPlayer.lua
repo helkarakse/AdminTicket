247a744b6c5decea5340bbf9afa57a0b69b7696a
helkarakse <nimcuron@gmail.com>
2013-12-20 10:38:07 +0800

    Updated startupPlayer.lua to download the player.lua file

--- src/startup/startupPlayer.lua
+++ src/startup/startupPlayer.lua
@@ -10,11 +10,11 @@ local fileArray = {
 	{link = "https://raw.github.com/helkarakse/LuaLibs/master/src/common/functions.lua", file = "functions"},
 	{link = "https://raw.github.com/helkarakse/LuaLibs/master/src/libs/libJson.lua", file = "json"},
 	{link = "https://raw.github.com/helkarakse/AdminTicket/develop/src/data.lua", file = "data"},
-	{link = "https://raw.github.com/helkarakse/AdminTicket/develop/src/main.lua", file = "main"},
+	{link = "https://raw.github.com/helkarakse/AdminTicket/develop/src/player.lua", file = "player"},
 }
 
 -- This filename is the file that will be executed
-local indexFile = "main"
+local indexFile = "player"
 
 -- Set to true to overwrite files
 local overwrite = true
