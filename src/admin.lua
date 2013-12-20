662bbf7d7b86d996639d8bb9a9ac444ef004f220
helkarakse <nimcuron@gmail.com>
2013-12-20 10:43:24 +0800

    Changed the event handler to rely on commandPrefix var from data

--- src/admin.lua
+++ src/admin.lua
@@ -21,3 +21,36 @@ local switch = functions.switch
 -- Variables
 local map
 local serverId = string.sub(os.getComputerLabel(), 1, 1)
+
+-- Event Handlers
+local chatEvent = function()
+	while true do
+		local _, username, message = os.pullEvent("chat_message")
+		-- check if the message is prefixed with a double //
+		if (message ~= nil) then
+			-- functions.debug("Message received by map peripheral: ", message)
+			if (string.sub(message, 1, string.len(data.commandPrefix)) == data.commandPrefix) then
+				-- strip the slash off the message and explode for args
+				-- replace spaces with + (spaces are not working for some reason)
+				local args = functions.explode("+", string.gsub(stripSlash(message), " ", "+"))
+				if (args[1] ~= "" and args[1] == "ticket") then
+					ticketHandler(username, message, args)
+				end
+			end
+		end
+	end
+end
+
+-- Main
+local function init()
+	local hasMap, mapDir = functions.locatePeripheral("adventure map interface")
+	if (hasMap) then
+		map = peripheral.wrap(mapDir)
+		functions.debug("Map peripheral detected and wrapped.")
+	else
+		functions.debug("No map peripheral detected. This is required.")
+		return
+	end
+
+	parallel.waitForAll(chatEvent)
+end
