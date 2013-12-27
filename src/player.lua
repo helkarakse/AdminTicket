f8a9e25ffd8061718b07a9ec12c863438a4cebf9
helkarakse <nimcuron@gmail.com>
2013-12-27 20:50:38 +0800

    Fixed the display of the user's ticket list

--- src/player.lua
+++ src/player.lua
@@ -223,8 +223,8 @@ local function ticketHandler(username, message, args)
 				if (functions.getTableCount(array.result) > 0) then
 					sendMessage(username, data.lang.myTickets)
 					for i = 1, functions.getTableCount(array.result) do
-						sendMessage(username, "#"..i.." - " .. functions.truncate(array.result[i].description, 25) .. " " 
-						.. string.upper(array.result[i].status) .. " [" .. array.result[i].time_ago .. "]")
+						sendMessage(username, "#" .. i .. " (" .. array.result[i].status .. ") - " .. functions.truncate(array.result[i].description, 25) ..
+							" - " .. array.result[i].time_ago)
 					end
 				else
 					sendMessage(username, data.lang.noTicketsFound)
